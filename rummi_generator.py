3fbb644c0c09c64da565e3866e84c7a0d605ca89
Romke <17853737+fRomke@users.noreply.github.com>
2019-12-11 14:31:03 +0100

    Writing results to txt file

--- rummi_generator.py
+++ rummi_generator.py
@@ -29,13 +29,21 @@ def paralellListWinningHands(minhand, maxhand, stones ,colors ,copies):
     stop = default_timer()
     printListWinningHands(result, round(stop - start, 2), minhand, stones, colors, copies)
 
+def writeResult(i, stones, colors, copies, r):
+    line = "n" + str(stones) + "k" + str(colors) + "m" + str(copies) + "h - " + str(i) + " - "
+    line += str(r[0]) + " - " + str(r[1]) + " - " + str(r[2])
+    out = open('results.txt', 'a')
+    out.write(line + '\n')
+    out.close()
+    return line
+
 # Multicoreprocessing per hand
 def paralellBigHands(minhand, maxhand, stones ,colors ,copies):
     r= []
     start = default_timer()
     for i in range(minhand, maxhand+1):
         r.append(rummi.perfCallRecCount(i, stones, colors, copies))
-        print(r[-1])
+        print(writeResult(i, stones, colors, copies, r[-1]))
     stop = default_timer()
     printListWinningHands(r, round(stop - start, 2), minhand, stones, colors, copies)
 
@@ -45,4 +53,4 @@ if __name__ == '__main__':
     #perfListWinningHands(3, 14, 13, 4, 2)
     #listWinningHands(3, 16, 6, 4, 2)
     #print(rummi.callRecCount(6, 13, 4, 2))
-    paralellBigHands(3, 14, 13, 4, 2)
+    paralellBigHands(3, 10, 6, 4, 2)
