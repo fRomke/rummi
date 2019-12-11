1c1751444c847fe76d0630095b6e5d8754f66c91
Romke <17853737+fRomke@users.noreply.github.com>
2019-12-11 12:00:19 +0100

    Comments and cleanup

--- rummi.py
+++ rummi.py
@@ -9,6 +9,8 @@ colors = "colors"
 copies = "copies"
 minimal_size = "minimal_size"
 
+#Returns all possible runs that can be layed on the table given table size, handsize and minimal run size
+#Not all options returned are necessarily allowed, this is determined by placeRun()
 def determinePossibleRuns(cfg, remaining_hand, run_size): #456
     options = []
     if (remaining_hand-run_size) >= cfg[minimal_size]:
@@ -18,6 +20,7 @@ def determinePossibleRuns(cfg, remaining_hand, run_size): #456
         options.append(remaining_hand)
     return options
 
+#Places the given run on the table; returns false when it fails
 def placeRun(cfg, table, i, row, run_size):
     for each in range(run_size):
         if table[row][each + i] == cfg[copies]:
@@ -26,6 +29,7 @@ def placeRun(cfg, table, i, row, run_size):
             table[row][each + i] += 1
     return table
 
+#Returns all possible group combinations that are allowed to be put on the table; returns false when it fails
 def determinePossibleGroups(cfg, table, col, group_size): #444
     group_list = [0,1,2,3]
     for i in range(cfg[colors]):
@@ -37,6 +41,7 @@ def determinePossibleGroups(cfg, table, col, group_size): #444
         group_list = findSubsets(group_list, group_size)
         return group_list
 
+#Places group on the table. No checks are done in this function, this is all done in determinePossibleGroups()
 def placeGroup(table, col, group):
     for i in group:
         table[i][col] += 1
@@ -55,11 +60,12 @@ def recursiveCount(args):
         options = determinePossibleRuns(cfg, remaining_hand, cfg[minimal_size])
         i_backup = i
         on_row_backup = on_row
-        for allowed_option in options: #stones=7 [3,4,7]
+        for allowed_option in options:
             i = i_backup
             on_row = on_row_backup
-            while on_row > - 1: #Checking runs for all the rows
-                while (cfg[stones] - i) >= allowed_option: #kolommen in de rij
+            #Looping over the rows
+            while on_row > - 1:
+                while (cfg[stones] - i) >= allowed_option: 
                     solutions = recursiveCount(
                         [on_row, 
                         i, 
@@ -71,8 +77,9 @@ def recursiveCount(args):
                 if on_row < (cfg[colors] - 1): on_row += 1
                 else: on_row = -1
                 i = 0
+            #Looping over the colums
             if allowed_option >= cfg[minimal_size] and allowed_option <= cfg[colors]:
-                while i != cfg[stones]:#kolom
+                while i != cfg[stones]:
                     group_options = determinePossibleGroups(cfg, table, i, allowed_option)
                     if group_options != False:
                         for g in group_options:
@@ -85,8 +92,9 @@ def recursiveCount(args):
                                 cfg])
                     i += 1
         return solutions
- 
 
+# Lists all recursive function calls for the first level of recursion. 
+# This can be easily return 
 def createTaskList(cfg, hand_size):
     table = initTable(cfg[colors], cfg[stones])
     solutions = set()
@@ -96,38 +104,36 @@ def createTaskList(cfg, hand_size):
     tasks = []
     for option in options:
         for on_row in on_rows:
+            #Task list for rows
             if on_row == -1 and option <= cfg[colors] :
                 for i in range(0, cfg[stones]):
                     for g in determinePossibleGroups(cfg, table, i, option):
                         tasks.append([on_row, i, hand_size-option, (placeGroup(copyTable(table), i, g)), solutions, cfg])
+            #Tasklist for columns
             elif on_row != -1 and option <= 5:
                 for i in range(0,cfg[stones]-option+1):
                     tasks.append([on_row, i, hand_size-option, placeRun(cfg, copyTable(table), i, on_row, option), solutions,cfg])
     return tasks
 
+# Wrapper for the recursive calls
+# Support for multicoreprocessing
 def perfCallRecCount(hand_size, nmax, k , m):
     #initializing vars
     config = {stones:nmax, colors:k, copies:m, minimal_size:3}
     pool = mp.Pool(mp.cpu_count()-1)
     tasks = createTaskList(config, hand_size)
-
-    #map unordered
+    #Calculation start
     start = default_timer()
     imapsol = set()
     for ip in pool.imap_unordered(recursiveCount, tasks):
         for i in ip:
             imapsol.add(i)
     stop = default_timer()
+    # Returns: Lengt of the solution set, time taken to calculate en storage size of the solution list
     return [len(imapsol), round(stop - start,2), placeValue((getsizeof(imapsol)))]
 
-    # start = default_timer()
-    # seqsol = set()
-    # for w in tasks:
-    #     seqsol = recursiveCount(w)
-    # print("seq (for loop)", len(seqsol))
-    # stop = default_timer()
-    # return (len(seqsol), round(stop - start,2))
-
+# Wrapper for the recursive calls
+# No support for multicoreprocessing
 def callRecCount(hand_size, nmax, k , m):
     config = {stones:nmax, colors:k, copies:m, minimal_size:3}
     table = initTable(colors, stones)
@@ -136,6 +142,3 @@ def callRecCount(hand_size, nmax, k , m):
     solutions = recursiveCount([0,0, hand_size, table, solutions, config])
     stop = default_timer()
     return (len(solutions), round(stop - start,2))
-
-if __name__ == '__main__':
-    perfCallRecCount(7,6,4,2)
--- rummi_generator.py
+++ rummi_generator.py
@@ -10,6 +10,7 @@ def printListWinningHands(result, t, minhand, stones ,colors ,copies):
         minhand += 1
     print("Total time taken: " + str(t)+ "s")
 
+# No multicoreprocessing
 def listWinningHands(minhand, maxhand, stones, colors, copies):
     result = []
     start = default_timer()
@@ -19,6 +20,7 @@ def listWinningHands(minhand, maxhand, stones, colors, copies):
     stop = default_timer()
     printListWinningHands(result, round(stop - start, 2), minhand, stones, colors, copies)
 
+# Calculates multiple hands at the same time.
 def paralellListWinningHands(minhand, maxhand, stones ,colors ,copies):
     pool  = mp.Pool(mp.cpu_count()-1)
     start = default_timer()
@@ -27,6 +29,7 @@ def paralellListWinningHands(minhand, maxhand, stones ,colors ,copies):
     stop = default_timer()
     printListWinningHands(result, round(stop - start, 2), minhand, stones, colors, copies)
 
+# Multicoreprocessing per hand
 def paralellBigHands(minhand, maxhand, stones ,colors ,copies):
     r= []
     start = default_timer()
@@ -43,113 +46,3 @@ if __name__ == '__main__':
     #listWinningHands(3, 16, 6, 4, 2)
     #print(rummi.callRecCount(6, 13, 4, 2))
     paralellBigHands(3, 14, 13, 4, 2)
-
-# perfListWinningHands()
-# For 4 colors with 6 stones and 2 copies of each:
-# 3 - 40 - 0.0s
-# 4 - 18 - 0.0s
-# 5 - 8 - 0.0s
-# 6 - 820 - 0.0s
-# 7 - 696 - 0.0s
-# 8 - 467 - 0.0s
-# 9 - 10872 - 0.05s
-# 10 - 12816 - 0.07s
-# 11 - 10896 - 0.06s
-# 12 - 103340 - 0.62s
-# 13 - 146760 - 1.02s
-# 14 - 144856 - 1.08s
-# 15 - 738648 - 5.23s
-# 16 - 1150642 - 9.51s
-# 17 - 1240616 - 11.33s
-# 18 - 4042944 - 36.29s
-# 19 - 6433240 - 68.16s
-# 20 - 7220872 - 88.11s
-# 21 - 16853400 - 199.74s
-# 22 - 25910748 - 371.19s
-# 23 - 29036248 - 486.24s
-# 24 - 52234799 - 877.57s
-# 25 - 74258224 - 1510.93s
-# 26 - 79916256 - 2002.55s
-# Total time taken: 2076.47s
-
-# For 4 colors with 7 stones and 2 copies of each:
-# 3 - 48 - 0.0s
-# 4 - 23 - 0.0s
-# 5 - 12 - 0.0s
-# 6 - 1176 - 0.01s
-# 7 - 1068 - 0.01s
-# 8 - 816 - 0.0s
-# 9 - 18912 - 0.1s
-# 10 - 24004 - 0.15s
-# 11 - 23088 - 0.15s
-# 12 - 222549 - 1.48s
-# 13 - 342624 - 2.64s
-# 14 - 382880 - 3.0s
-# 15 - 2023488 - 15.19s
-# 16 - 3441477 - 29.46s
-# 17 - 4225324 - 38.22s
-# 18 - 14567450 - 126.35s
-# 19 - 25528004 - 257.2s
-# 20 - 32957292 - 357.87s
-# 21 - 83299676 - 895.29s
-# Total time taken: 903.53s
-
-# For 4 colors with 13 stones and 1 copies of each:
-# 3 - 96 - 0.0s
-# 4 - 53 - 0.0s
-# 5 - 36 - 0.0s
-# 6 - 4010 - 0.03s
-# 7 - 4100 - 0.03s
-# 8 - 3690 - 0.03s
-# 9 - 97624 - 0.71s
-# 10 - 136694 - 1.1s
-# 11 - 147204 - 1.26s
-# 12 - 1567624 - 12.14s
-# 13 - 2610728 - 22.5s
-# 14 - 3152280 - 28.79s
-# 15 - 17734816 - 145.86s
-# 16 - 31972283 - 294.53s
-# 17 - 41233308 - 422.33s
-# Total time taken: 425.67s
-
-# For 2 colors with 13 stones and 2 copies of each:
-# 3 - 22 - 0.0s
-# 4 - 20 - 0.0s
-# 5 - 18 - 0.0s
-# 6 - 253 - 0.0s
-# 7 - 426 - 0.0s
-# 8 - 564 - 0.0s
-# 9 - 2216 - 0.01s
-# 10 - 4621 - 0.03s
-# 11 - 7498 - 0.05s
-# 12 - 17280 - 0.12s
-# 13 - 35082 - 0.29s
-# 14 - 61067 - 0.56s
-# 15 - 113868 - 1.12s
-# 16 - 207577 - 2.26s
-# 17 - 352328 - 4.17s
-# 18 - 590790 - 7.85s
-# 19 - 970294 - 14.59s
-# 20 - 1533963 - 26.81s
-# 21 - 2358978 - 47.62s
-# 22 - 3528600 - 82.76s
-# 23 - 5111852 - 141.17s
-# 24 - 7182257 - 232.97s
-# 25 - 9781466 - 382.83s
-# 26 - 12886595 - 611.83s
-# Total time taken: 627.03s
-
-# For 4 colors with 13 stones and 2 copies of each:
-# 3 - 96 - 0.05s
-# 4 - 53 - 0.03s
-# 5 - 36 - 0.05s
-# 6 - 4656 - 0.06s
-# 7 - 4980 - 0.06s
-# 8 - 4731 - 0.06s
-# 9 - 151728 - 0.22s
-# 10 - 233412 - 0.33s
-# 11 - 279108 - 0.39s
-# 12 - 3753318 - 4.52s
-# 13 - 7244080 - 9.83s
-# 14 - 10232524 - 14.43s
-# 15 - 75493324 - 99.68s
