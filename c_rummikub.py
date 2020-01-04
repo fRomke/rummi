06f7c5051e36fa36c7c8bdc511ee759fd3e5ff68
fRomke <17853737+fRomke@users.noreply.github.com>
2020-01-04 16:21:32 +0100

    Splitting and reorder of functions

--- c_rummikub.py
+++ c_rummikub.py
@@ -1,5 +1,6 @@
 class cRummikub:
     import subprocess
+    import multiprocessing as mp
     def __init__(self):
         self.inlist = []
         self.outlist = []
@@ -7,11 +8,40 @@ class cRummikub:
         # TODO build frank.cc
     
     def __del__(self):
-        self.subprocess.call(["rm", "in/*.in"])
+        #elf.subprocess.call(["ls", "in"])
+        #self.subprocess.call(["rm", "in/*.in"])
+        pass
 
     def appendGame(self, summed, l):
         self.inlist.append(l)
         self.matchlist.append(summed)
+
+    def buildAndRunMP(self):
+        self.buildMP()
+        self.runMP()
+        return self.isWinning()
+
+    def buildMP(self):
+        from math import ceil
+        self.cores = self.mp.cpu_count()-1
+        length = len(self.inlist)
+        chunk = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+        self.partsMP = chunk(self.inlist, ceil(length / self.cores))
+        
+    def runMP(self):
+        from itertools import chain
+        pool = self.mp.Pool(self.cores)
+        result = pool.map(self.buildAndRun, self.partsMP)
+        pool.close()
+        pool.join()
+        self.outlist = list(chain(*result))
+
+    def buildAndRun(self, l):
+        import os
+        print(os.getpid())
+        ifile = "in/" + str(os.getpid()) + ".in"
+        self.build(ifile, l)
+        return self.run(ifile)
 
     def build(self, ifile, l):
         f = open(ifile, 'w')
@@ -20,34 +50,6 @@ class cRummikub:
             f.write(str(each[0]) + '\n')
             f.write(str(each[1]) + '\n')
         f.close
-    
-    def buildMP(self):
-        import multiprocessing as mp
-        from itertools import chain
-        from math import ceil
-        length = len(self.inlist)
-        chunk = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
-        cores = mp.cpu_count()-1
-        pool = mp.Pool(cores)
-        print(cores)
-        parts = chunk(self.inlist, ceil(length / cores))
-        result = pool.map(self.buildAndRunMP, parts)
-        b= list(chain(*result))
-        print(result)
-
-    # TODO seperate buildMP into buildandrun etc
-
-    def buildAndRunMP(self, l):
-        import os
-        print(os.getpid())
-        ifile = "in/" + str(os.getpid()) + ".in"
-        self.build(ifile, l)
-        return self.run(ifile)
-
-    def buildAndRun(self, option):
-        ifile = "in/1.in"
-        self.build(ifile, self.inlist)
-        return self.run(ifile)
 
     def run(self, ifile):
         MyOut = self.subprocess.Popen(["./frank", ifile], 
