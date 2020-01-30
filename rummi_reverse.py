53e13e731a1d1bced43cb57b48862ca0e8ad6b68
fRomke <17853737+fRomke@users.noreply.github.com>
2020-01-30 23:07:01 +0100

    Huge perf boost by removing dups

--- rummi_reverse.py
+++ rummi_reverse.py
@@ -1,5 +1,5 @@
 from rummi_util import initTable, memoryUsage
-from rummi_output import writeResult
+from rummi_output import writeResult, printTableToConsole
 from itertools import chain
 import c_rummikub
 from sys import argv
@@ -26,6 +26,53 @@ def findSubsets(solutions, reference, to_remove, i = 0):
                     print("Finding subsets milestone ", len(solutions))
         i += 1
 
+def removeDups(solutions):
+    def twoDimensionize(table):
+        chunk = 0
+        stones = int(len(table)/colors)
+        table_2D = []
+        for c in range(colors):
+            table_2D.append(table[chunk:(chunk+stones)])
+            chunk += stones
+        return table_2D
+    
+    def flipOverY(table):
+        newtable_2D = []
+        for each in twoDimensionize(table):
+            each.reverse()
+            newtable_2D.append(each)
+        return list(chain(*newtable_2D))
+
+    def flipOverX(table):
+        newtable_2D = twoDimensionize(table)
+        newtable_2D.reverse()
+        return list(chain(*newtable_2D))
+
+    def flipOverXY(table):
+        newtable = table[:]
+        newtable.reverse()
+        return newtable
+
+    def findAndDel(it, count):
+        try:
+            id = solutions.index(it)
+            del solutions[id]
+            return 1
+        except ValueError:
+            return 0
+    
+    count = 0
+    multisol = []
+    while(solutions):
+        sol = solutions.pop(0)
+        multiplier = 1
+        multiplier += findAndDel(flipOverX(sol), count)
+        multiplier += findAndDel(flipOverXY(sol), count)
+        multiplier += findAndDel(flipOverY(sol), count)
+        multisol.append((sol, multiplier))
+        count += 1
+    return multisol
+
 def reverseCount(hand, stones, colors, copies, cores):
     # Initialize variables
     maxhand = stones * copies * colors
@@ -43,12 +90,17 @@ def reverseCount(hand, stones, colors, copies, cores):
     # Find unique subsets
     findSubsets(solutions, table, to_remove)
     subset = default_timer()
-    print("Found", len(solutions), "possible subsets. Time taken:", round(subset - init,2), "\nExecuting...")
+    print("Found", len(solutions), "possible subsets. Time taken:", round(subset - init,2), "\nRemoving duplicates...")
+
+    # Removing duplicates
+    solutions = removeDups(solutions)
+    dups = default_timer()
+    print(len(solutions), "possible subsets left. Time taken:", round(dups - subset,2), "\nExecuting...")
 
     # Execute the determined situations
-    result = None#cR.delegate(solutions)
+    result = cR.delegate(solutions)
     execution = default_timer()
-    print("Execution done. Time taken:", round(subset - execution,2))
+    print("Execution done. Time taken:", round(execution - dups,2))
 
     # Finalizing
     stop = default_timer()
@@ -58,9 +110,9 @@ def reverseCount(hand, stones, colors, copies, cores):
 
 if __name__ == '__main__':
     stones = 6
-    maxhand = 46
-    minhand = 46#maxhand - 1
-    cores = 4
+    maxhand = 45
+    minhand = 45#maxhand - 1
+    cores = 7
     if len(argv) == 5:
         maxhand = int(argv[1])
         minhand = int(argv[2])
