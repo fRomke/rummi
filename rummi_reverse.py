2230a4600cd18d5cdf143a3b045b96929b549757
fRomke <17853737+fRomke@users.noreply.github.com>
2020-01-30 23:16:34 +0100

    Removed a print

--- rummi_reverse.py
+++ rummi_reverse.py
@@ -22,8 +22,6 @@ def findSubsets(solutions, reference, to_remove, i = 0):
                 findSubsets(solutions, copy[:], (to_remove - each), i+1)
             else:
                 solutions.append(copy)
-                if(len(solutions)%1000 == 0):
-                    print("Finding subsets milestone ", len(solutions))
         i += 1
 
 def removeDups(solutions):
