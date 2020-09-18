77f552402c12616be7a4d2ca839f154f75fc5cd8
fRomke <17853737+fRomke@users.noreply.github.com>
2020-09-18 11:42:32 +0200

    Updated variables to match the environment

--- decrypt_hash.py
+++ decrypt_hash.py
@@ -1,5 +1,9 @@
-import rummi4
-hash = 105
+stones = "stones"
+colors = "colors"
+copies = "copies"
+minimal_size = "minimal_size"
+config = {stones:6, colors:4, copies:2, minimal_size:3}
+hash = 680
 
 def printTafel(tafel):
     if tafel == False:
@@ -11,10 +15,10 @@ def printTafel(tafel):
 
 l = []
 j = 0
-for c in range(colors):
+for c in range(config[colors]):
     t = []
-    for i in range(n):
-        value = ((cijfer & (3 << j))>>j) #3 = 0b11 oftewel het masker
+    for i in range(config[stones]):
+        value = ((hash & (3 << j))>>j) #3 = 0b11 oftewel het masker
         t.append(value)
         j += 2
     l.append(t)
